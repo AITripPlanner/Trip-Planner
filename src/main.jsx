4a248c6b093f1a01001a665c674a1e667ff57d05
DevdattKhilari <devdattkhilari@gmail.com>
2024-09-12 16:11:09 +0530

    add

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import './index.css';
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import CreateTrip from './create-trip';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>
+  },
+  {
+    path:'/create-trip',
+    element:<CreateTrip/>
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
