e6a38f507eeca2dec81a23c2e89045b5200366b1
DevdattKhilari <112195580+DevdattKhilari@users.noreply.github.com>
2024-09-20 16:08:21 +0530

    Update Header.jsx

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import { Button } from '../ui/button';
+
+const Header = () => {
+  return (
+    <>
+    <div className=' shadow-sm flex items-center '>
+      <img src='/logo.svg' alt='Logo' className='w-36 h-20 ml-10  ' />
+      <div className='absolute right-4'>
+        <Button>Sign In</Button>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Header;
 
